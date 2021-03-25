501c05d335b88e1b0a970f71467ad15024cb70fe
原罪 <531362022@qq.com>
2021-03-25 08:43:56 +0800

    chore: Build examples automatically

--- scripts/buildDemo.sh
+++ scripts/buildDemo.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+version=$(cat ./package.json | grep '"version"' | cut -d":" -f2 | sed 's/ "\(.*\)",/\1/')
+
 for path in $(ls examples)
 do
   cd examples/$path
-  yarn
+  yarn add vite-react@^$version --dev
   yarn build
   cd -
 done
