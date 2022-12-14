2c2849d3d151f3bda8595d91728d23dfa36a0e18
geekact <fanwenhua1990@gmail.com>
2022-12-14 16:20:27 +0800

    feat: update css modules scope name

--- src/handler/css.ts
+++ src/handler/css.ts
@@ -6,5 +6,5 @@ export const handleCss = (config: Config, env: ConfigEnv) => {
   if (config.css.modules === false) return;
   config.css.modules ||= {};
   config.css.modules.generateScopedName ??=
-    env.command === 'build' ? '[hash:base64]' : '[path][name]__[local]';
+    env.command === 'build' ? '[hash:base64]' : '[path][name]-[ext]___[local]';
 };
